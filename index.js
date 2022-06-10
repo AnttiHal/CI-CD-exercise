5d6344cb60f267de2d8673f855a5cc307659fa9c
AnttiHalmetoja <anttihalmetoja@gmail.com>
2022-06-10 09:30:09 +0300

    eslint

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-const { response } = require('express')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
@@ -20,8 +19,8 @@ app.use(morgan(':body'))
 
 
 
-let persons = [
-    {
+/*let persons = [
+  {
       "id": 1,
       "name": "Antti Halmetoja",
       "number": "04457023485"
@@ -42,39 +41,39 @@ let persons = [
       "number": "1234567"
     }
   ]
-
+*/
 app.get('/api/persons', (req, res) => {
   Person.find({}).then(persons => {
     res.json(persons)
   })
 })
 
-app.get('/api/info', (req, res, next) => {    
-    Person.find({}).then(persons => {
-      res.send(`<p>${new Date()} <br> Phonebook has info for  ${persons.length}   people.</p> `)}
-    )
+app.get('/api/info', (req, res, next) => {
+  Person.find({}).then(persons => {
+    res.send(`<p>${new Date()} <br> Phonebook has info for  ${persons.length}   people.</p> `)}
+  )
     .catch (error => next(error))
 
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
-  .then(person => {
-    if (person) {
-      res.json(person)
-    } else {
-      res.status(404).end()
-    }
-  }) 
-  .catch (error => next(error)) 
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch (error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
-  .then(result => {
-    res.status(204).end()
-  })
-  .catch(error => next(error))
+    .then(res => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 
@@ -84,28 +83,28 @@ const generateId = () => {
   return id
 }
 
-app.post('/api/persons', (req,res, next) => {  
+app.post('/api/persons', (req,res, next) => {
   const body = req.body
   if (body.name === ''|| body.number === '') {
     return res.status(400).json({
       error: 'Name or number missing'
     })
   }
-  if (persons.find(person => person.name === body.name)) {
+  /*if (persons.find(person => person.name === body.name)) {
     return res.status(400).json({
       error: 'Name already exists. Pick a new name.'
     })
-  }
+  }*/
   const person = new Person({
     id: generateId(),
     name: body.name,
     number: body.number
   })
-  
+
   person.save().then(savedPerson => {
     res.json(savedPerson)
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 // middleware virheiden käsittelyyn
@@ -115,7 +114,7 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error : 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
-    }
+  }
   next(error)
 }
 
