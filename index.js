7d3fd320029ed8b17d141cf7e136b0aa5b0979e2
AnttiHalmetoja <anttihalmetoja@gmail.com>
2022-08-31 07:51:02 +0300

    add health check endpoint

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use(express.static('./client/build'))
 morgan.token('body', (req,res) => {
   return req.method + res.statusCode + req.url + JSON.stringify(req.body)}
 )
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(morgan(':body'))
